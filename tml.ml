38e7d0a7fb211b97c863c06b8d9557cb10debdd5
capybara84 <eitoshi.ashihara@gmail.com>
2020-02-18 00:41:51 +0900

    Add top_level

--- tml.ml
+++ tml.ml
@@ -1,3 +1,21 @@
+open Syntax
+
+let env = []
+
+let rec top_level () =
+    try
+        print_string "> ";
+        flush stdout;
+        let line = input_line stdin in
+        let v = Eval.eval env @@ Parser.parse_one @@ Scanner.from_string line in
+        print_endline @@ value_to_str v;
+        top_level ()
+    with
+        | Error s -> (print_endline s; top_level ())
+        | Sys_error s -> print_endline s
+        | End_of_file -> ()
 
 let () =
-    Test.test()
+    Test.test();
+    top_level()
+
